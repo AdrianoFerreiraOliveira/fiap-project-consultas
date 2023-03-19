257a4a5dec0546298964e1b41235f38f43edf9e9
AdrianoOliveira <105682437+AdrianoFerreiraOliveira@users.noreply.github.com>
2023-03-19 20:58:01 -0300

    Create Authentication

--- RestConsulta/views.py
+++ RestConsulta/views.py
@@ -1,11 +1,17 @@
 from rest_framework import viewsets
 from RestConsulta.models import Medico, Paciente
 from RestConsulta.serializer import MedicoSerializer, PacienteSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
 
 class MedicosViewSet(viewsets.ModelViewSet):
     """ Exibindo a lista de médicos"""
     queryset = Medico.objects.all()
     serializer_class = MedicoSerializer
+    authentication_classes =[BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class PacientesViewSet(viewsets.ModelViewSet):
     """ Exibindo a lista de pacientes"""
